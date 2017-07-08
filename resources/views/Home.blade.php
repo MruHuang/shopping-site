957a553feb60c9c0ddadbf15852eb70a9adb7671
monkeyteacher <peter1180041@gmail.com>
2017-07-09 00:07:49 +0800

    更改會員需知(Register)&增加ATM資訊(home)

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -8,7 +8,10 @@
 
 @section('content')
 	@include('partials.commodity_area_3')
-	
+	<div class='alert alert-info col-xs-12' >
+		ATM匯款方式<br/>銀行代號：808<br/>存戶帳號：0129-940-053850<br/>玉山銀行雙和分行 <br/>
+		戶名：藍星購物商行趙淑芬
+	<div>
 @stop
 
 {{--@if(count($message_text))--}}
