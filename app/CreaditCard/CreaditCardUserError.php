d512f63fc6407bc8239294de499b2aa6e4a23303
mru <mruskylink@gmail.com>
2017-07-10 01:54:23 +0800

    信用卡檔案建置

---
+++ app/CreaditCard/CreaditCardUserError.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\CreaditCard;
+
+class CreaditCardTransactionComplete
+
+{
+    public function TransactionComplete(
+        $ONO,
+        $LTD,
+        $LTT,
+        $RNN,
+        $AIR,
+        $AN
+    ){
+    	
+    }
+}
