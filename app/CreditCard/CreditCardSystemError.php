be63f2f371f34352d821ca3f2c88b78a64a2e748
mru <mruskylink@gmail.com>
2017-07-15 16:27:41 +0800

    信用卡-頁面轉調至訂單查詢。交易失敗更新訂單標號亂數

--- app/CreditCard/CreditCardSystemError.php
+++ app/CreditCard/CreditCardSystemError.php
@@ -21,7 +21,22 @@ class CreditCardSystemError
             $MID,
             $ONO
         );
-        $resultMessage = '系統錯誤';
-        return $resultMessage;
+        $resultMessage = '系統錯誤(';
+        
+        if($RC == 'G6')
+            $resultMessage+="訂單編號重複";
+        else if($RC == 'G0')
+             $resultMessage+="系統功能有誤";
+        else if($RC == 'G1')
+             $resultMessage+="交易逾時";
+        else if($RC == 'G2')
+             $resultMessage+="資料格式錯誤";
+        else if($RC == 'G5')
+             $resultMessage+="非使用中特定店";
+        else if($RC == 'G9')
+             $resultMessage+="Session檢查有誤";
+        $random_number = strval(time()).str_random(5);
+        $this->ccSQL->ChangeOrderONO($ONO,$random_number);
+        return $resultMessage.')';
     }
 }
